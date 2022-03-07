07236edbf034e8dd9498edff00d0f82824c594ba
furkancelen <furkan.celen@boun.edu.tr>
2022-03-07 17:52:19 +0300

    helva pişti

---
+++ quiz.py
@@ -0,0 +1,8 @@
+def allsubstrings(name_str):
+    letter = []
+    ascii = []
+    name_str = input()
+    for i in name_str:
+        letter.append(i)
+        ascii.append(ord(i))
+        return 
