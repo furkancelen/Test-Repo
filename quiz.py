7532aaf09aadf9ddca7ca0146bd24f78d54aa1d2
furkancelen <furkan.celen@boun.edu.tr>
2022-04-29 21:02:10 +0300

    commit

--- quiz.py
+++
@@ -1,8 +0,0 @@
-def allsubstrings(name_str):
-    letter = []
-    ascii = []
-    name_str = input()
-    for i in name_str:
-        letter.append(i)
-        ascii.append(ord(i))
-        return name_str
