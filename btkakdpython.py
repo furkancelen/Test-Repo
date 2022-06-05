66f4468df1289d0436a085113e7cf92d28f26ed0
furkancelen <furkan.celen@boun.edu.tr>
2022-06-05 18:19:49 +0300

    object oriented programming btk akademi

---
+++ btkakdpython.py
@@ -0,0 +1,93 @@
+# # class Person:
+# #     # class attributes
+# #     address = "not available"
+
+# #     # constructor
+# #     def __init__(self, name, year):
+
+# #         # object attributes
+# #         self.name = name
+# #         self.year = year
+
+# #     #instance methods
+# #     def intro(self):
+# #         print("Hello World. I am " + self.name)
+
+# #     def calculateAge(self):
+# #         return 2022 - self.year
+
+
+# # p1 = Person(name ="Furkan", year= 2000)
+# # p2 = Person(name = "Nebiye", year = 2000)
+
+# # print(f"{p1.name} is {p1.calculateAge()} years old.")
+# # print(f"{p2.name} is {p2.calculateAge()} years old")
+
+
+
+
+# class Circle:
+#     # Class object attribute
+#     pi = 3.14
+
+#     def __init__(self, radius = 1):
+#         self.radius = radius
+    
+#     # Methods
+#     def calcCircumference(self):
+#         return 2 * self.pi * self.radius
+
+#     def calcArea(self):
+#         return self.pi * (self.radius)**2 
+
+# p1 = Circle()
+# p2 = Circle(5)
+
+# print(f"The circumference of the circle is {p1.calcCircumference()} meters")
+# print(f"The circumference of the circle is {p2.calcCircumference()} meters")
+
+# print(f"The area of the circle is {p1.calcArea()} metersquares")
+# print(f"The area of the circle is {p2.calcArea()} metersquares")
+
+
+#INHERITANCE
+
+class Person():
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+        print("Person created")
+    def who_am_i(self):
+        print("I am a person")
+
+
+class Student(Person):
+    def __init__(self, fname, lname, number):
+        Person.__init__(self, fname, lname) # equivalent to super().__init__(fname, lname)
+        self.studentnumber = number
+        print("Student created")
+
+    #overriding 
+    def who_am_i(self):
+        print("I am a student")
+
+    def say_hello(self):
+        print("Hello I am a student")
+
+class Teacher(Person):
+    def __init__(self, fname, lname, branch):
+        super().__init__(fname, lname)
+        self.branch = branch
+    
+    def who_am_i(self):
+        print(f"I am a {self.branch} teacher")
+
+
+x = Person("Furkan", "Çelen")
+y = Student("Nebiye", "Yavuz", 1269)
+z = Teacher("Ali Tamer", "Ünal", "IE" )
+
+print( x.firstname + " " + x.lastname )
+print( y.firstname + " " + y.lastname + " " + str(y.studentnumber))
+print( z.firstname + " " + z.lastname + " " + str(z.who_am_i()))
+
--- learn.py
+++
@@ -1,4 +0,0 @@
-x = "I am learning how to add a phyton file into git repository"
-y = "Let's find out"
-
-print(x + y)
